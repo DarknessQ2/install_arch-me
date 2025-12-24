a426dd83f06941c1623b4df60b024d183724f90e
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 01:56:28 -0700

    Enhance base.sh with additional packages and configurations
    Updated the pacstrap command to include additional packages for audio, video, and desktop environment support. Configured timezone, locale, hostname, network, audio services, and GRUB installation in the chroot environment.

--- base.sh
+++ base.sh
@@ -1,22 +1,51 @@
 #!/bin/bash
 set -e
 
-pacstrap /mnt base linux linux-firmware networkmanager grub os-prober sudo nano
+pacstrap /mnt \
+base linux linux-firmware \
+networkmanager \
+sudo nano git \
+grub os-prober \
+pipewire pipewire-alsa pipewire-pulse pipewire-jack \
+wireplumber \
+pavucontrol \
+mesa libdrm \
+vulkan-intel \
+xf86-video-intel \
+hyprland wayland \
+xdg-desktop-portal-hyprland \
+kitty foot \
+wlroots \
+xdg-user-dirs
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash <<EOF
+
+# Zona horaria
 ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 hwclock --systohc
 
+# Locale
 sed -i 's/#es_MX.UTF-8 UTF-8/es_MX.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=es_MX.UTF-8" > /etc/locale.conf
 
+# Hostname
 echo "archlinux" > /etc/hostname
 
+# Red
 systemctl enable NetworkManager
 
+# Audio
+systemctl enable pipewire pipewire-pulse wireplumber
+
+# GRUB BIOS
 grub-install --target=i386-pc $DISK
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Directorios usuario
+xdg-user-dirs-update
+
 EOF
+
