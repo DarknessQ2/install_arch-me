31e4bd4ec4a9f665db7ad23516e3d24f4d77a753
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 02:15:52 -0700

    Log disk used for installation in .disk file
    Add a line to record the disk used for installation.

--- disk.sh
+++ disk.sh
@@ -2,14 +2,13 @@
 set -e
 
 if [ -d /sys/firmware/efi ]; then
-    echo "❌ UEFI detectado. Este instalador es SOLO BIOS."
-    exit 1
+  echo "❌ UEFI detectado. Este instalador es SOLO BIOS."
+  exit 1
 fi
 
 lsblk
 read -rp "Disco (ej: /dev/sda): " DISK
 read -rp "ESCRIBE BORRAR para continuar: " OK
-
 [ "$OK" != "BORRAR" ] && exit 1
 
 umount -R /mnt 2>/dev/null || true
@@ -20,3 +19,5 @@ parted -s "$DISK" mkpart primary ext4 1MiB 100%
 
 mkfs.ext4 -F "${DISK}1"
 mount "${DISK}1" /mnt
+
+echo "$DISK" > /mnt/.disk
