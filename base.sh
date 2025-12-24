b7730cc16577377d56112628449ee378578c4f55
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 02:28:39 -0700

    Modify base.sh for English locale and neovim
    Updated base.sh to install neovim instead of nano and changed locale settings from Spanish to English.

--- base.sh
+++ base.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
 set -e
 
-DISK=$(cat /mnt/.disk)
-
 pacstrap /mnt \
-base linux linux-firmware intel-ucode \
-networkmanager \
-sudo nano git \
-grub os-prober \
-pipewire pipewire-alsa pipewire-pulse pipewire-jack \
-wireplumber pavucontrol \
-mesa libdrm xf86-video-intel libva-utils \
-hyprland wayland wlroots xdg-desktop-portal-hyprland \
-kitty foot \
-xdg-user-dirs \
-bluez-utils \
-ufw \
-zram-generator
+base linux linux-firmware \
+grub networkmanager \
+pipewire pipewire-alsa pipewire-pulse wireplumber \
+intel-ucode sof-firmware \
+sudo nano neovim
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt /bin/bash <<EOF
+arch-chroot /mnt /bin/bash << EOF
 ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 hwclock --systohc
 
-sed -i 's/#es_MX.UTF-8 UTF-8/es_MX.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
-echo "LANG=es_MX.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-echo "archlinux" > /etc/hostname
+echo "arch" > /etc/hostname
 
 systemctl enable NetworkManager
-systemctl enable pipewire pipewire-pulse wireplumber
-systemctl enable bluetooth
-systemctl enable ufw
-
-grub-install --target=i386-pc $DISK
-grub-mkconfig -o /boot/grub/grub.cfg
-
-xdg-user-dirs-update
 EOF
 
