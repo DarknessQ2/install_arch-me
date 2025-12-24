938872d7c64cd760da570cc252db898fa55b69d3
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 01:42:00 -0700

    Refactor disk.sh for improved disk setup process

--- disk.sh
+++ disk.sh
@@ -2,19 +2,18 @@
 set -e
 
 lsblk
-read -p "Disco (ej: /dev/nvme0n1): " DISK
-read -p "ESCRIBE BORRAR para continuar: " OK
+read -rp "Disco (ej: /dev/sda): " DISK
+read -rp "ESCRIBE BORRAR para continuar: " OK
 
 [ "$OK" != "BORRAR" ] && exit 1
 
-parted -s $DISK mklabel gpt
-parted -s $DISK mkpart EFI fat32 1MiB 513MiB
-parted -s $DISK set 1 esp on
-parted -s $DISK mkpart ROOT ext4 513MiB 100%
+umount -R /mnt 2>/dev/null || true
 
-mkfs.fat -F32 ${DISK}1
-mkfs.ext4 ${DISK}2
+wipefs -af "$DISK"
 
-mount ${DISK}2 /mnt
-mkdir -p /mnt/boot
-mount ${DISK}1 /mnt/boot
+parted -s "$DISK" mklabel msdos
+parted -s "$DISK" mkpart primary ext4 1MiB 100%
+
+mkfs.ext4 -F "${DISK}1"
+
+mount "${DISK}1" /mnt
