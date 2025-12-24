25b68fa2499a888a72a47ef29696efe502a8b170
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 01:44:57 -0700

    Revise base.sh for improved system setup
    Updated base installation script to include essential packages and configuration steps.

--- base.sh
+++ base.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e
 
-pacstrap /mnt \
-base linux linux-firmware sudo networkmanager \
-git fish flatpak pipewire pipewire-pulse wireplumber \
-hyprland waybar kitty grim slurp wl-clipboard \
-xdg-desktop-portal-hyprland pavucontrol
+pacstrap /mnt base linux linux-firmware networkmanager grub os-prober sudo nano
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "root:root" | arch-chroot /mnt chpasswd
+arch-chroot /mnt /bin/bash <<EOF
+ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
+hwclock --systohc
 
-arch-chroot /mnt systemctl enable NetworkManager
+sed -i 's/#es_MX.UTF-8 UTF-8/es_MX.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=es_MX.UTF-8" > /etc/locale.conf
+
+echo "archlinux" > /etc/hostname
+
+systemctl enable NetworkManager
+
+grub-install --target=i386-pc $DISK
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
