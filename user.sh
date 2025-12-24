065d148fc3d92f8a48be710c7454d68398a194f5
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 02:14:38 -0700

    Enhance user setup and package installation process
    Updated user.sh to add user setup, package installations, and Flatpak applications.

--- user.sh
+++ user.sh
@@ -4,37 +4,59 @@ set -e
 USERNAME="luis"
 PASSWORD="luis"
 
+# ---- Todo esto corre DENTRO del sistema instalado ----
 arch-chroot /mnt /bin/bash <<EOF
 
 # Usuario
 useradd -m -G wheel $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
 
-# Sudo seguro
+# Sudo (seguro, sin duplicar líneas)
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-# Paquetes necesarios
+# Paquetes base para usuario
 pacman -S --noconfirm --needed \
-flatpak \
 git \
+base-devel \
+flatpak \
 fish
 
-# Flatpak
+# Flatpak repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Apps Flatpak
-flatpak install -y flathub \
-com.discordapp.Discord \
-org.mozilla.firefox \
-com.google.Chrome \
-com.valvesoftware.Steam \
-org.prismlauncher.PrismLauncher \
-org.kde.krita \
-org.libreoffice.LibreOffice
 
 EOF
 
-# Caelestia como usuario
+# ---- Instalar YAY como usuario ----
+arch-chroot /mnt sudo -u "$USERNAME" bash <<EOF
+cd /home/$USERNAME
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -rf yay
+EOF
+
+# ---- Apps AUR ----
+arch-chroot /mnt sudo -u "$USERNAME" bash <<EOF
+yay -S --noconfirm \
+google-chrome \
+github-desktop-bin \
+ipscan-bin
+EOF
+
+# ---- Apps Flatpak ----
+arch-chroot /mnt /bin/bash <<EOF
+flatpak install -y flathub \
+com.discordapp.Discord \
+com.valvesoftware.Steam \
+io.github.unknownskl.geforcenow-electron \
+org.DolphinEmu.dolphin-emu \
+org.prismlauncher.PrismLauncher \
+org.vinegarhq.Sober
+EOF
+
+# ---- Caelestia ----
 arch-chroot /mnt sudo -u "$USERNAME" bash <<EOF
 git clone https://github.com/caelestia-dots/caelestia.git /home/$USERNAME/.local/share/caelestia
 fish /home/$USERNAME/.local/share/caelestia/install.fish
