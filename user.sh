1d74ab74e4ed2c2125e68a4b839ef3dca0c5b4b7
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 01:47:32 -0700

    Refactor user creation and package installation
    Refactor user creation and package installation in user.sh. Use variables for username and password, and ensure proper sudo configuration.

--- user.sh
+++ user.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -e
 
-useradd -m luis
-echo "luis:luis" | chpasswd
-usermod -aG wheel luis
+USERNAME="luis"
+PASSWORD="luis"
 
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+arch-chroot /mnt /bin/bash <<EOF
 
-su - luis << 'EOF'
+# Usuario
+useradd -m -G wheel $USERNAME
+echo "$USERNAME:$PASSWORD" | chpasswd
 
+# Sudo seguro
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+# Paquetes necesarios
+pacman -S --noconfirm --needed \
+flatpak \
+git \
+fish
+
+# Flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Apps Flatpak
 flatpak install -y flathub \
 com.discordapp.Discord \
 org.mozilla.firefox \
@@ -20,8 +32,10 @@ org.prismlauncher.PrismLauncher \
 org.kde.krita \
 org.libreoffice.LibreOffice
 
-git clone https://github.com/caelestia-dots/caelestia.git ~/.local/share/caelestia
-fish ~/.local/share/caelestia/install.fish
-
 EOF
 
+# Caelestia como usuario
+arch-chroot /mnt sudo -u "$USERNAME" bash <<EOF
+git clone https://github.com/caelestia-dots/caelestia.git /home/$USERNAME/.local/share/caelestia
+fish /home/$USERNAME/.local/share/caelestia/install.fish
+EOF
