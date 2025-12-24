fbb4c6191744831999c1f5be2024b3611fb93b96
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 02:16:24 -0700

    Modify base.sh for improved package setup
    Updated package installation and system configuration.

--- base.sh
+++ base.sh
@@ -1,51 +1,43 @@
 #!/bin/bash
 set -e
 
+DISK=$(cat /mnt/.disk)
+
 pacstrap /mnt \
-base linux linux-firmware \
+base linux linux-firmware intel-ucode \
 networkmanager \
 sudo nano git \
 grub os-prober \
 pipewire pipewire-alsa pipewire-pulse pipewire-jack \
-wireplumber \
-pavucontrol \
-mesa libdrm \
-vulkan-intel \
-xf86-video-intel \
-hyprland wayland \
-xdg-desktop-portal-hyprland \
+wireplumber pavucontrol \
+mesa libdrm xf86-video-intel libva-utils \
+hyprland wayland wlroots xdg-desktop-portal-hyprland \
 kitty foot \
-wlroots \
-xdg-user-dirs
+xdg-user-dirs \
+bluez-utils \
+ufw \
+zram-generator
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash <<EOF
-
-# Zona horaria
 ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 hwclock --systohc
 
-# Locale
 sed -i 's/#es_MX.UTF-8 UTF-8/es_MX.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=es_MX.UTF-8" > /etc/locale.conf
 
-# Hostname
 echo "archlinux" > /etc/hostname
 
-# Red
 systemctl enable NetworkManager
-
-# Audio
 systemctl enable pipewire pipewire-pulse wireplumber
+systemctl enable bluetooth
+systemctl enable ufw
 
-# GRUB BIOS
 grub-install --target=i386-pc $DISK
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Directorios usuario
 xdg-user-dirs-update
-
 EOF
 
