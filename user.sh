0fa461254ae532a64cb7c583188db562b8ed2739
DarknessQ2 <123284762+DarknessQ2@users.noreply.github.com>
2025-12-24 02:30:56 -0700

    Refactor user.sh for user creation and package installation

--- user.sh
+++ user.sh
@@ -1,65 +1,37 @@
 #!/bin/bash
 set -e
 
-USERNAME="luis"
-PASSWORD="12345"
+arch-chroot /mnt /bin/bash << 'EOF'
+useradd -m luis
+echo "luis:luis" | chpasswd
+usermod -aG wheel luis
 
-arch-chroot /mnt /bin/bash <<EOF
-useradd -m -G wheel $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+chmod 440 /etc/sudoers.d/wheel
 
-pacman -S --noconfirm --needed \
-base-devel \
-git \
-flatpak \
-fish \
-htop neofetch neovim \
-wget curl fd fzf lf \
-cava mangohud \
-gparted \
-udiskie \
-terminus-font \
-alsa-utils sof-firmware \
-mesa-utils mpvpaper swww \
-xarchiver file-roller ark \
-firefox krita libreoffice-fresh \
-network-manager-applet
+pacman -S --noconfirm git flatpak fish
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-EOF
-
-# yay
-arch-chroot /mnt sudo -u "$USERNAME" bash <<EOF
-cd /home/$USERNAME
+su - luis << 'EOT'
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
-rm -rf yay
-EOF
 
-# AUR apps
-arch-chroot /mnt sudo -u "$USERNAME" bash <<EOF
-yay -S --noconfirm \
-google-chrome \
-github-desktop-bin \
-ipscan-bin
-EOF
+flatpak remote-add --if-not-exists flathub \
+https://flathub.org/repo/flathub.flatpakrepo
 
-# Flatpak apps
-arch-chroot /mnt /bin/bash <<EOF
 flatpak install -y flathub \
 com.discordapp.Discord \
+org.mozilla.firefox \
+com.google.Chrome \
 com.valvesoftware.Steam \
-io.github.unknownskl.geforcenow-electron \
-org.DolphinEmu.dolphin-emu \
 org.prismlauncher.PrismLauncher \
+org.kde.krita \
+org.libreoffice.LibreOffice \
+org.DolphinEmu.dolphin-emu \
 org.vinegarhq.Sober
-EOF
 
-# Caelestia
-arch-chroot /mnt sudo -u "$USERNAME" bash <<EOF
-git clone https://github.com/caelestia-dots/caelestia.git /home/$USERNAME/.local/share/caelestia
-fish /home/$USERNAME/.local/share/caelestia/install.fish
+git clone https://github.com/caelestia-dots/caelestia.git ~/.local/share/caelestia
+fish ~/.local/share/caelestia/install.fish
+EOT
 EOF
